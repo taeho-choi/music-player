694b748d6c649bbb8c966a553aa598ac2e7e8d02
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-03-02 19:01:36 +0900

    Play, Pause, Load function / Image rotate animation

--- js/main.js
+++ js/main.js
@@ -9,4 +9,22 @@ for(let el of lists) {
     el.style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
     pic.style.backgroundImage = `url(../img/member${i+1}.jpg)`;
     i++;
+
+    let play = el.querySelector(".play");
+    let pause = el.querySelector(".pause");
+    let load = el.querySelector(".load");
+
+    play.addEventListener("click", e=> {
+        e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+        e.currentTarget.closest("article").querySelector("audio").play();
+    });
+    pause.addEventListener("click", e=> {
+        e.currentTarget.closest("article").querySelector(".pic").classList.remove("on");
+        e.currentTarget.closest("article").querySelector("audio").pause();
+    });
+    load.addEventListener("click", e=> {
+        e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+        e.currentTarget.closest("article").querySelector("audio").load();
+        e.currentTarget.closest("article").querySelector("audio").play();
+    });
 }
