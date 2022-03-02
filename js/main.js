5db57e61668336e66ec4164d155c93542eae708d
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-03-02 18:45:57 +0900

    Album cover image, text

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const len = lists.length - 1;
 let i = 0;
 
 for(let el of lists) {
-    el.style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
+    let pic = el.querySelector(".pic");
+    el.style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
+    pic.style.backgroundImage = `url(../img/member${i+1}.jpg)`;
     i++;
 }
